0ec76fb1afc1dd897812815b203ffecc9b999291
Nils <area51.nils@googlemail.com>
2014-08-03 17:09:27 +0200

    implemented a clickable album

--- script/script.js
+++ script/script.js
@@ -1,1 +1,28 @@
-// This is thejavascript 
+// This is the javascript 
+$(document).ready(function() {
+	// Dateinamen auslesen
+	var files = new Array("IMGP5364_Kaktus-mit-Augen.jpg", "sdjfkjsbd.jpg");
+	var dates = new Array("00.00.00", "00.00.00");
+	
+	// Bilder einfügen
+	var html = ""
+	for(row = 0; row < 10; row++) {
+		
+		html += "<tr>";
+		for(col = 0; col < 5; col++) {
+			if(files.length > row*5 + col) {
+				html += "<td>" + 
+					"<a href=\"data/" + files[row*5 + col] + "\" data-lightbox=\"image\">" + 
+					"<div class=\"item\"><img src=\"data/" + files[row*5 + col] + "\"></img><p>" + dates[row*5 + col] + "</p></div>" + 
+					"</a></td>";
+			}
+			else {
+				html += "<td></td>";
+			}
+		}
+		html += "</tr>";
+	}
+	
+	$("#container").html(html);
+		
+});
